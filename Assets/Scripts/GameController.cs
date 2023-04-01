22a92795ca032ffe378927d3dd881fb23f737236
Jaibeer  S Dugal <jaibeer72@gmail.com>
2023-04-02 05:01:52 +0530

    Connecting main menu to the main game and reset button

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,7 +7,7 @@ public class GameController : MonoBehaviour
     [SerializeField]
     public GameObject player;
     public bool isDebug = false;
-    public GameObject timeInDay;
+    public GameObject timeInDay = null;
 
     // state machine fields
     public static GameController Instance { get; private set; }
@@ -33,7 +33,6 @@ public class GameController : MonoBehaviour
     {
         //initialise the player object
         Instantiate(player, transform.position, transform.rotation, this.transform);
-        Instantiate(timeInDay, transform.position, transform.rotation, this.transform);
         changeState.AddListener(HandeStateChange);
 
         // initialises the current state to the home state on start-up
@@ -41,7 +40,7 @@ public class GameController : MonoBehaviour
         {
             return;
         }
-        currentState = new HomeState();
+        currentState = new MainMenuState();
 
         currentState.OnEnter();
     }
@@ -67,6 +66,17 @@ public class GameController : MonoBehaviour
     {
         if (newState != oldState)
         {
+            if (newState.GetType().Name == "HomeState" )
+            {
+                if(!timeInDay.scene.IsValid())
+                    Instantiate(timeInDay, transform.position, transform.rotation, this.transform);
+                else
+                {
+                    // timer reset
+                    timeInDay.GetComponent<TimeManger>().resetTimer();
+                }
+              
+            }
             currentState = newState;
             currentState.OnEnter();
         }
