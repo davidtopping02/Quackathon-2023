d0340c09c327179ee9092a186fbefc7ba5a02567
David Topping <71976336+davidtopping02@users.noreply.github.com>
2023-04-01 22:38:46 +0100

    added day timer functionality

--- Assets/Scripts/TimeManger.cs
+++ Assets/Scripts/TimeManger.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
@@ -7,23 +5,33 @@ public class TimeManger : MonoBehaviour
 {
     public TextMeshProUGUI m_TextMeshPro;
     [SerializeField]
-    public uint m_UpHours = 6; 
-    float timeLeft; 
+    public uint m_UpHours = 6;
+    float timeLeft;
 
     // Start is called before the first frame update
     void Start()
     {
-        timeLeft = m_UpHours;     
+        timeLeft = m_UpHours;
     }
 
     // Update is called once per frame
     void Update()
     {
-        timeLeft -= Time.deltaTime / 60.0f; // Subtract one minute in real time
+        timeLeft -= Time.deltaTime / 1.0f; // Subtract one minute in real time
 
         if (timeLeft <= (0.1 * m_UpHours))
         {
-            // Ha ha I am in danger... 
+            // penalise the player
+            PlayerStatsEventArgs args = new PlayerStatsEventArgs(PlayerStatsEventArgs.cmd.DecreaseStreangth, 5);
+            GameController.Instance.player.GetComponent<PlayerStats>().StatsChangeEvent.Invoke(args);
+            args = new PlayerStatsEventArgs(PlayerStatsEventArgs.cmd.DecreaseSocial, 5);
+            GameController.Instance.player.GetComponent<PlayerStats>().StatsChangeEvent.Invoke(args);
+            args = new PlayerStatsEventArgs(PlayerStatsEventArgs.cmd.DecreaseFood, 5);
+            GameController.Instance.player.GetComponent<PlayerStats>().StatsChangeEvent.Invoke(args);
+
+            // Invoke the event with a string parameter
+            State endstate = new EndOfDayState();
+            GameController.Instance.changeState.Invoke(endstate);
         }
 
         // Update the timer text
